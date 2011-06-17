6265ea995ffdc9f14c67c390f59afa201c050edf
Kent Fredric <kentfredric@gmail.com>
2011-06-18 09:57:35 +1200

    eliminate excess whitespace

--- lib/MooseX/Attribute/ValidateWithException.pm
+++ lib/MooseX/Attribute/ValidateWithException.pm
@@ -3,9 +3,70 @@ use warnings;
 
 package MooseX::Attribute::ValidateWithException;
 
-# ABSTRACT:
+# ABSTRACT: Cause validation failures to throw exception objects.
 
 use Moose;
+
+=head1 DESCRIPTION
+
+B<ALPHA QUALITY SOFTWARE>.
+
+At present, when an attribute fails validation, Moose internally die()'s with a
+string. There is also no way to throw an exception object as part of the
+validation message, ( in order to give more context on the problem ), without
+also breaking how much of the validation works.
+
+This module is an experiment in providing that feature, which really should be
+done in Moose itself, and done better, which is why it has been given such an
+obtuse name.
+
+This module makes no promises of forwards compatibility with a future Moose
+release, in order to permit Moose to do whatever they want and not worry about
+"breaking code" that uses this module. ( So that they can easily replace this
+module in incompatible ways )
+
+B<< If your code breaking is unacceptable, do I<not> use this module >>.
+
+Use of this module assumes several things.
+
+=over 4
+
+=item 1. You are o.k. with your code breaking in a future Moose release.
+
+=item 2. You are o.k. with re-writing any and all code that depends on this
+functionality, if a future Moose release is incompatible with this module.
+
+=back
+
+I'm not saying I won't do my best to provide forwards compatibility, but it is
+highly unlikely it will be possible, due to differences in package naming
+which may be essential for handling exceptions.
+
+=head1 SYNOPSIS
+
+  {
+    package Foo;
+    use Moose;
+    use MooseX::Attribute::ValidateWithException;
+
+    has foo => (
+      isa => 'Str',
+      is  => 'rw',
+      required => 1,
+    );
+    __PACKAGE__->meta->make_immutable;
+    no Moose;
+  }
+
+  use Try::Tiny;
+
+  try {
+    Foo->new( foo => { this_is => [qw( not what we were wanting )] } );
+  } catch {
+    say $_->name if blessed( $_ ) && $_->isa('Thingy');
+  };
+
+=cut
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
