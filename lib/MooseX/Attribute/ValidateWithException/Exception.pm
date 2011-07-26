e73746fbab80bb847748e68f7bedb8b795c668b4
Kent Fredric <kentfredric@gmail.com>
2011-07-26 12:07:21 +1200

    fix issues with Moose 2.020xxx

--- lib/MooseX/Attribute/ValidateWithException/Exception.pm
+++ lib/MooseX/Attribute/ValidateWithException/Exception.pm
@@ -27,10 +27,10 @@ has 'constraint_message' => (
   required => 1,
 );
 
-has 'constraint' => (
-  is       => 'ro',
-  required => 1,
-);
+# has 'constraint' => (
+#  is       => 'ro',
+#  required => 1,
+#);
 
 has 'constraint_name' => (
   isa      => 'Str',
